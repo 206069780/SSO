eb7472ed792b9b4953f3db8d8f3eea5022718225
Your Name <you@example.com>
2023-04-05 02:21:57 +0800

    token工具类和webConfig抽取

---
+++ src/main/java/com/senrui/sso/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.senrui.sso.config;
+
+import com.senrui.core.cofing.CoreWebConfig;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends CoreWebConfig {
+
+}
